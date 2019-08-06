39259adbbbe1026528694a140e7cd70abfdba3d0
Ashley Lewis <ashleymichal@gmail.com>
2019-08-06 17:27:25 -0500

    fix #12: Add body to `workerskv::RenameNamespace`

--- src/workerskv/rename_namespace.rs
+++ src/workerskv/rename_namespace.rs
@@ -6,9 +6,10 @@ use crate::endpoint::{Endpoint, Method};
 pub struct RenameNamespace<'a> {
     pub account_identifier: &'a str,
     pub namespace_identifier: &'a str,
+    pub params: RenameNamespaceParams,
 }
 
-impl<'a> Endpoint<(), RenameNamespaceParams> for RenameNamespace<'a> {
+impl<'a> Endpoint<(), (), RenameNamespaceParams> for RenameNamespace<'a> {
     fn method(&self) -> Method {
         Method::Put
     }
@@ -17,6 +18,9 @@ impl<'a> Endpoint<(), RenameNamespaceParams> for RenameNamespace<'a> {
             "accounts/{}/storage/kv/namespaces/{}",
             self.account_identifier, self.namespace_identifier
         )
+    }
+    fn body(&self) -> Option<RenameNamespaceParams> {
+        Some(self.params.clone())
     }
 }
 
