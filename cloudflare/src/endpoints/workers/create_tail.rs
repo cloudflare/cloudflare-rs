f46964b6fa0c8369a3be99868da5dffd70184339
Ian Spivey <ispivey@gmail.com>
2020-02-15 11:25:59 -0600

    Fixes #IDK. Adds endpoints for Workers Tail resources.

---
+++ cloudflare/src/endpoints/workers/create_tail.rs
@@ -0,0 +1,35 @@
+use super::WorkersTail;
+
+use crate::framework::endpoint::{Endpoint, Method};
+
+/// Create Tail
+/// https://api.cloudflare.com/#worker-create-tail
+pub struct CreateTail<'a> {
+    /// Account ID of owner of the script
+    pub account_identifier: &'a str,
+    /// The name of the script to tail
+    pub script_name: &'a str,
+    /// URL to which to send events
+    pub params: CreateTailParams,
+}
+
+impl<'a> Endpoint<WorkersTail, (), CreateTailParams> for CreateTail<'a> {
+    fn method(&self) -> Method {
+        Method::Post
+    }
+    fn path(&self) -> String {
+        format!(
+            "accounts/{}/workers/scripts/{}/tails",
+            self.account_identifier, self.script_name
+        )
+    }
+    fn body(&self) -> Option<CreateTailParams> {
+        Some(self.params.clone())
+    }
+}
+
+#[derive(Serialize, Clone, Debug)]
+pub struct CreateTailParams {
+    /// URL to which to send events
+    pub url: String,
+}
---
+++ cloudflare/src/endpoints/workers/create_tail_heartbeat.rs
@@ -0,0 +1,26 @@
+use super::WorkersTail;
+
+use crate::framework::endpoint::{Endpoint, Method};
+
+/// Create Tail
+/// https://api.cloudflare.com/#worker-create-tail
+pub struct CreateTailHeartbeat<'a> {
+    /// Account ID of owner of the script
+    pub account_identifier: &'a str,
+    /// The name of the script to tail
+    pub script_name: &'a str,
+    /// The unique identifier of the Tail session
+    pub tail_id: &'a str,
+}
+
+impl<'a> Endpoint<WorkersTail> for CreateTailHeartbeat<'a> {
+    fn method(&self) -> Method {
+        Method::Post
+    }
+    fn path(&self) -> String {
+        format!(
+            "accounts/{}/workers/scripts/{}/tails/{}/heartbeat",
+            self.account_identifier, self.script_name, self.tail_id
+        )
+    }
+}
