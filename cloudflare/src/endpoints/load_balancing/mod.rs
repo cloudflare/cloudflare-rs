dbd4f26564457913d924c4b41b2e441c26e403e1
Nuno Diegues <diegues.nuno@gmail.com>
2021-04-19 18:05:34 +0100

    Add create/delete/list for Load Balancers (#139)
    Co-authored-by: Nuno Diegues <nuno@cloudflare.com>

--- cloudflare/src/endpoints/load_balancing/mod.rs
+++ cloudflare/src/endpoints/load_balancing/mod.rs
@@ -1,13 +1,114 @@
+pub mod create_lb;
 pub mod create_pool;
+pub mod delete_lb;
 pub mod delete_pool;
+pub mod list_lb;
 pub mod pool_details;
 
 use crate::framework::response::ApiResult;
 use chrono::offset::Utc;
 use chrono::DateTime;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::hash::{Hash, Hasher};
 use std::net::IpAddr;
+
+#[derive(Eq, PartialEq, Deserialize, Serialize, Clone, Debug)]
+pub struct LoadBalancer {
+    pub id: String,
+    pub created_on: DateTime<Utc>,
+    pub modified_on: DateTime<Utc>,
+    pub description: String,
+    /// The DNS hostname to associate with your Load Balancer. If this hostname already exists as a
+    /// DNS record in Cloudflare's DNS, the Load Balancer will take precedence and the DNS record
+    /// will not be used.
+    pub name: String,
+    pub enabled: bool,
+    /// Time to live (TTL) of the DNS entry for the IP address returned by this load balancer. This
+    /// only applies to gray-clouded (unproxied) load balancers.
+    #[serde(default = "LoadBalancer::default_ttl")]
+    pub ttl: u32,
+    /// The pool ID to use when all other pools are detected as unhealthy.
+    pub fallback_pool: LbPoolId,
+    /// A list of pool IDs ordered by their failover priority. Pools defined here are used by
+    /// default, or when region_pools are not configured for a given region.
+    pub default_pools: Vec<LbPoolId>,
+    /// A mapping of region/country codes to a list of pool IDs (ordered by their failover priority)
+    /// for the given region. Any regions not explicitly defined will fall back to using
+    /// default_pools.
+    pub region_pools: LbPoolMapping,
+    /// A mapping of Cloudflare PoP identifiers to a list of pool IDs (ordered by their failover
+    /// priority) for the PoP (datacenter). Any PoPs not explicitly defined will fall back to using
+    /// default_pools.
+    pub pop_pools: LbPoolMapping,
+    pub proxied: bool,
+    pub steering_policy: SteeringPolicy,
+    pub session_affinity: SessionAffinity,
+    pub session_affinity_attributes: SessionAffinityAttributes,
+    #[serde(default = "LoadBalancer::default_session_affinity_ttl")]
+    pub session_affinity_ttl: u32,
+}
+
+impl LoadBalancer {
+    fn default_ttl() -> u32 {
+        30
+    }
+    fn default_session_affinity_ttl() -> u32 {
+        5000
+    }
+}
+
+type LbPoolId = String;
+type LbPoolMapping = HashMap<String, Vec<LbPoolId>>;
+
+#[derive(Eq, PartialEq, Deserialize, Serialize, Clone, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum SteeringPolicy {
+    /// Empty policy maps to `Geo` if `region_pools` or `pop_pools` are used, or otherwise `Off`.
+    #[serde(rename = "")]
+    Nil,
+    Off,
+    Geo,
+    Random,
+    DynamicLatency,
+}
+
+#[derive(Eq, PartialEq, Deserialize, Serialize, Clone, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum SessionAffinity {
+    /// Empty has the same behaviour as `None`.
+    #[serde(rename = "")]
+    Nil,
+    None,
+    Cookie,
+    IpCookie
+}
+
+#[derive(Eq, PartialEq, Deserialize, Serialize, Clone, Debug)]
+pub struct SessionAffinityAttributes {
+    pub samesite: SameSite,
+    pub secure: Secure,
+    pub drain_duration: u32,
+}
+
+#[derive(Eq, PartialEq, Deserialize, Serialize, Clone, Debug)]
+pub enum SameSite {
+    /// `Auto` maps to `Lax` if Always Use HTTPS is set, or `None` otherwise.
+    Auto,
+    Lax,
+    None,
+    Strict,
+}
+
+#[derive(Eq, PartialEq, Deserialize, Serialize, Clone, Debug)]
+pub enum Secure {
+    /// `Auto` maps to `Always` if Always Use HTTPS is set, or `Never` otherwise.
+    Auto,
+    Always,
+    Never
+}
+
+impl ApiResult for LoadBalancer {}
+
 
 /// A pool is a set of origins that requests could be routed to (e.g. each of your data centers or
 /// regions have its own pool).
