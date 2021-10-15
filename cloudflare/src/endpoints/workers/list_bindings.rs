ff11f3b6ff67fb270809d13cd21cccd5c08a2503
ocs <46542932+ocsfrank@users.noreply.github.com>
2021-10-15 08:19:36 -0700

    Wrangler delete endpoints (#166)
    * add delete_script
    
    * add endpoints for binding and deleting DO
    
    update delete_script
    
    add list_scripts
    
    add bindings and do
    
    update namings
    
    remove endpoints

---
+++ cloudflare/src/endpoints/workers/list_bindings.rs
@@ -0,0 +1,26 @@
+use crate::framework::endpoint::{Endpoint, Method};
+use super::WorkersBinding; 
+
+/// List Bindings
+/// Lists all bindings for a given script
+#[derive(Debug)]
+pub struct ListBindings<'a> {
+    /// account id of owner of the script
+    pub account_id: &'a str,
+    /// name of script to list bindings for
+    pub script_name: &'a str,
+}
+
+impl<'a> Endpoint<Vec<WorkersBinding>> for ListBindings<'a> {
+    fn method(&self) -> Method {
+        Method::Get
+    }
+
+    fn path(&self) -> String {
+        format!(
+            "accounts/{}/workers/scripts/{}/bindings",
+            self.account_id,
+            self.script_name
+        )
+    }
+}
