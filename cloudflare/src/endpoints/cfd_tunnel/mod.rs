4309cfe9079b14e9f2b8c57f36646ecd13a00062
Arturo Guerra <ar2roguerra@gmail.com>
2024-12-09 16:14:50 -0600

    Adds support for cfd_tunnel configuration endpoints

--- cloudflare/src/endpoints/cfd_tunnel/mod.rs
+++ cloudflare/src/endpoints/cfd_tunnel/mod.rs
@@ -1,8 +1,10 @@
 pub mod create_tunnel;
 mod data_structures;
 pub mod delete_tunnel;
+pub mod get_configuration;
 pub mod list_tunnels;
 pub mod route_dns;
+pub mod update_configuration;
 pub mod update_tunnel;
 
 pub use data_structures::*;
