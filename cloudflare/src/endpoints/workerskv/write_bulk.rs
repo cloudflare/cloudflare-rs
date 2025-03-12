12799a82b0bd47b3f8b146d65e17cf7cd9e5f997
Gonçalo <GoncaloGarcia@users.noreply.github.com>
2025-03-12 04:53:21 +0000

    chore: Remove auto_added field as its no longer included in the DNS response (#269)

--- cloudflare/src/endpoints/workerskv/write_bulk.rs
+++ cloudflare/src/endpoints/workerskv/write_bulk.rs
@@ -7,13 +7,13 @@ use serde::{Deserialize, Serialize};
 /// Write multiple keys and values at once.
 ///
 /// Body should be an array of up to 10,000 key-value pairs to be stored, along with optional expiration information.
-/// 
+///
 /// Existing values and expirations will be overwritten.
 /// If neither expiration nor expiration_ttl is specified, the key-value pair will never expire.
 /// If both are set, expiration_ttl is used and expiration is ignored.
-/// 
+///
 /// The entire request size must be 100 megabytes or less.
-/// 
+///
 /// A `404` is returned if a write action is for a namespace ID the account doesn't have.
 ///
 /// <https://developers.cloudflare.com/api/resources/kv/subresources/namespaces/methods/bulk_update/>
